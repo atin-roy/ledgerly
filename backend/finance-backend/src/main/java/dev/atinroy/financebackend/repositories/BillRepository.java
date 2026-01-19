d82810c0581bb73bc3f8f239a361fe079ceee94a
Atin Roy <me@atinroy.dev>
2026-01-19 20:08:46 +0530

    feat(bill, budget): add repositories for Bill & Budget

--- backend/finance-backend/src/main/java/dev/atinroy/financebackend/repositories/BillRepository.java
+++ backend/finance-backend/src/main/java/dev/atinroy/financebackend/repositories/BillRepository.java
@@ -1,7 +1,6 @@
 package dev.atinroy.financebackend.repositories;
 
 import dev.atinroy.financebackend.entity.Bill;
-import dev.atinroy.financebackend.repositories.BillSuggestion;
 import dev.atinroy.financebackend.entity.BillStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * - Pagination-first (no unbounded queries)
@@ -23,13 +23,13 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
      * This is intentionally the simplest query.
      * Other filters build conceptually on top of this.
      */
-    Page<Bill> findByUser_Id(Long userId, Pageable pageable);
+    Page<Bill> findByUser_UserId(Long userId, Pageable pageable);
 
     /**
      * Fetch bills for a user filtered by name.
      * Uses fuzzy search
      */
-    Page<BillSuggestion> findByUser_IdAndBillNameContainingIgnoreCase(
+    Page<BillSuggestion> findByUser_UserIdAndBillNameContainingIgnoreCase(
         Long userId,
         String query,
         Pageable pageable
@@ -66,7 +66,7 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
      * Status enums are stable and index-friendly.
      * This query scales well even with large datasets.
      */
-    Page<Bill> findByUser_IdAndBillStatus(
+    Page<Bill> findByUser_UserIdAndBillStatus(
         Long userId,
         BillStatus billStatus,
         Pageable pageable
@@ -83,10 +83,10 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
      * BETWEEN is inclusive:
      *   startDate <= createdAt <= endDate
      */
-    Page<Bill> findByUser_IdAndCreatedAtBetween(
+    Page<Bill> findByUser_UserIdAndCreatedAtBetween(
         Long userId,
-        LocalDate startDate,
-        LocalDate endDate,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
         Pageable pageable
     );
 }
--- backend/finance-backend/src/main/java/dev/atinroy/financebackend/repositories/BudgetRepository.java
+++ backend/finance-backend/src/main/java/dev/atinroy/financebackend/repositories/BudgetRepository.java
@@ -1,4 +1,9 @@
 package dev.atinroy.financebackend.repositories;
 
+import dev.atinroy.financebackend.entity.Budget;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface BudgetRepository {
+    Page<Budget> findByUser_UserId(Long userId, Pageable pageable);
 }
