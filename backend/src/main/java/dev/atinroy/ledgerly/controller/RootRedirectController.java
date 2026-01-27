adbc0ba8b06d8d96824a20ae697d8aa1af756804
Atin Roy <me@atinroy.dev>
2026-01-27 19:39:20 +0530

    chore: redirect home to swagger ui

---
+++ backend/src/main/java/dev/atinroy/ledgerly/controller/RootRedirectController.java
@@ -0,0 +1,12 @@
+package dev.atinroy.ledgerly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootRedirectController {
+    @GetMapping("/")
+    public String redirectToSwaggerUi() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
